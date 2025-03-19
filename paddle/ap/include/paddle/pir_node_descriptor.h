77abe48dd1ee8838fb2416c26921416b0253bf74
hxzd5568 <3257591325@qq.com>
2025-03-19 11:07:39 +0800

    support meticulous matching (with input/outoput number)

--- paddle/ap/include/paddle/pir_node_descriptor.h
+++ paddle/ap/include/paddle/pir_node_descriptor.h
@@ -22,6 +22,7 @@
 #include "paddle/ap/include/ir_match/ref_match_ctx.h"
 #include "paddle/ap/include/paddle/pir/packed_ir_op_inner_source_pattern_helper.h"
 #include "paddle/ap/include/paddle/pir_node.h"
+#include "paddle/ap/include/drr/drr_graph_descriptor.h"
 
 namespace ap::paddle {
 
@@ -172,8 +173,41 @@ struct PirNodeDescriptor {
     return true;
   }
 
+  adt::Result<std::size_t> GetDrrNumInputs(const drr::PackedIrOp<drr::Node>& drr_op) {
+    return ap::drr::DefaultDrrGraphDescriptor{}.GetNumInputs(drr_op);
+  }
+
+  adt::Result<std::size_t> GetDrrNumOutputs(const drr::PackedIrOp<drr::Node>& drr_op) {
+    return ap::drr::DefaultDrrGraphDescriptor{}.GetNumOutputs(drr_op);
+  }
+
+  adt::Result<std::size_t> GetPirNumInputs(const PackedIrOp& pir_op) {
+    std::size_t size = ap::paddle::GetUsedExternalValue(*pir_op.fusion_op).size();
+    return size;
+  }
+
+  adt::Result<std::size_t> GetPirNumOutputs(const PackedIrOp& pir_op) {
+    for (auto& op : *pir_op.fusion_op.block()) {
+      if (op.isa<pir::YieldOp>()) {
+        std::size_t size = op.num_operands();
+        return size;
+      }
+    }
+    return adt::errors::TypeError{"no cf.yield found in FusionOp"};
+  }
+
   adt::Result<bool> PackedOpAttrsSatisfy(
       const PackedIrOp& pir_op, const drr::PackedIrOp<drr::Node>& drr_op) {
+    ADT_LET_CONST_REF(drr_num_inputs, GetDrrNumInputs(drr_op));
+    ADT_LET_CONST_REF(pir_num_inputs, GetPirNumInputs(pir_op));
+    if (drr_num_inputs != pir_num_inputs) {
+      return false;
+    }
+    ADT_LET_CONST_REF(drr_num_outputs, GetDrrNumOutputs(drr_op));
+    ADT_LET_CONST_REF(pir_num_outputs, GetPirNumOutputs(pir_op));
+    if (drr_num_outputs != pir_num_outputs) {
+      return false;
+    }
     ADT_LET_CONST_REF(inner_source_pattern_satisfy,
                       PackedOpInnerSourcePatternSatisfy(pir_op, drr_op));
     if (!inner_source_pattern_satisfy) {
