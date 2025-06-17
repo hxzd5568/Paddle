bca26bc38c5667a80e55774993a1ddea71d6a304
hxzd5568 <3257591325@qq.com>
2025-06-17 11:21:54 +0800

    [Big tensor] check type for mean op

--- paddle/phi/kernels/reduce_mean_kernel.cc
+++ paddle/phi/kernels/reduce_mean_kernel.cc
@@ -26,6 +26,11 @@ void MeanKernel(const Context& dev_ctx,
                 const IntArray& dims,
                 bool keep_dim,
                 DenseTensor* out) {
+  bool valid_arg = x.dtype() == DataType::FLOAT16 || x.dtype() == DataType::FLOAT32 ||
+                    x.dtype() == DataType::FLOAT64 || x.dtype() == DataType::COMPLEX64 ||
+                    x.dtype() == DataType::COMPLEX128;
+  PADDLE_ENFORCE(valid_arg,phi::errors::InvalidArgument(
+      "The DataType of mean Op must be either a floating point or complex dtype. But got: %s", DataTypeToString(x.dtype())));
   bool reduce_all = recompute_reduce_all(x, dims);
   MeanRawKernel<T>(dev_ctx, x, dims, keep_dim, reduce_all, out);
 }
